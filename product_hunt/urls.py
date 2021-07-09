5a858523fad85c6abf12463e3aec940efb5f754b
shubhammlk75 <45136076+shubhammlk75@users.noreply.github.com>
2021-07-09 14:46:49 +0530

    Add files via upload

---
+++ product_hunt/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from products import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('account/',include('account.urls')),
+    path('product/',include('products.urls')),
+    
+]
